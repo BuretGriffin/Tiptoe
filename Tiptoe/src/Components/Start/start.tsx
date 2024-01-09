e137a2832cd3e8c821c6f795255d7af0760e17f9
Griffin <buretgriffin01@gmail.com>
2024-01-09 13:03:11 +0300

    start fixed

---
+++ Tiptoe/src/Components/Start/start.tsx
@@ -0,0 +1,86 @@
+import { useGameStore } from "../../util/useGameStore";
+
+import {
+    Button,
+    Wrapper,
+    Square,
+    Emoji,
+    Title,
+    ButtonsContainer,
+    Footer,
+    FooterLink ,
+}
+from"./styles";
+import { getRandomEmoji } from "../../util";
+import React, { useState,useEffect } from "react";
+import { TypeAnimation } from "react-type-animation";
+export default function Start(){
+    const {setNavigate,setGameNavigate}=useGameStore();
+    const{emoji,setEmoji}=useState(getRandomEmoji());
+
+    
+    useEffect(()=>{
+        const interval= setInterval(()=>{
+            setEmoji(getRandomEmoji());
+        },1500);
+
+        return()=>{
+            clearInterval(interval);
+        };
+    },[]);
+
+    const handleOnClick=()=>{
+        setNavigate("multi-player");
+        setGameNavigate("multi-player");
+
+    };
+    const handleOnSinglePlayerClick = () => {
+        setNavigate("single-player");
+        setGameNavigate("select-box");
+      };
+      return (
+        <Wrapper
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5 }}
+          exit={{ opacity: 0 }}
+        >
+          <Square
+            className="container"
+            initial={{ scale: 0 }}
+            animate={{ rotate: 180, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 260,
+              damping: 20,
+            }}
+          >
+            <Emoji>{emoji}</Emoji>
+          </Square>
+          <Title>
+            Tic Tac&nbsp;
+            <TypeAnimation
+              sequence={["Toe", 2000, "Emoji", 2000]}
+              wrapper="span"
+              speed={5}
+              repeat={Infinity}
+            />
+          </Title>
+          <ButtonsContainer>
+            <Button onClick={handleOnSinglePlayerClick}>Single Player</Button>
+            <Button onClick={handleOnClick}>Multi Player</Button>
+          </ButtonsContainer>
+          <Footer>
+            <FooterLink
+              href="https://github.com/BuretGriffin/Tiptoe"
+              target="_blank"
+            >
+              Made with love by Griff🤍
+            </FooterLink>
+          </Footer>
+        </Wrapper>
+      );
+
+}
+
+
